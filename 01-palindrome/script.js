3b77a2ef23ce02ed61741e01376ff4c7374b5302
Marina Kravchenko <marishka-k87@yandex.ru>
2022-01-26 22:43:53 +0300

    1 готова

--- 01-palindrome/script.js
+++ 01-palindrome/script.js
@@ -7,26 +7,36 @@
  *
  * Напишите функцию palindrome(str), принимающую как аргумент строку.
  * Функция должна вернуть true, если строка — палиндром, и false, если нет.
- * 
+ *
  * Считайте, что на вход всегда передаётся слово: то есть знаков препинания
  * и пробелов в аргументе быть не может.
- * 
-*/
+ *
+ */
 
 function palindrome(str) {
-    // Напишите код здесь
+  if (typeof str === "string") {
+    const strLetters = str.replaceAll(/[^А-яЁёA-Za-z]/g, "");
+    let a = strLetters.toLowerCase().split("", strLetters.length);
+    if (a.join("") === a.reverse().join("")) {
+      return true;
+    } else {
+      return false;
+    }
+  } else return "Введите строку";
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(palindrome('топот')); // должно быть true
-console.log(palindrome('Saippuakivikauppias')); // true
-console.log(palindrome('привет')); // false
+console.log(palindrome("топот")); // должно быть true
+console.log(palindrome("Saippuakivikauppias")); // true
+console.log(palindrome("привет")); // false
+console.log(palindrome("О, лета тело!")); // true
+console.log(palindrome("А роза упала на лапу Азора")); // true
 
 /*
  * Бонус. Задача для любознательных. Пусть функция принимает на вход любую строку,
  * но пробелы и знаки препинания не учитывает. Например:
- * 
+ *
  * palindrome('О, лета тело!'); // true
- * 
-*/
+ *
+ */
