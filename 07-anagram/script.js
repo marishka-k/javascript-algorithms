36ddaca944ae962f9f800d400a1bd251850eafc2
Marina Kravchenko <marishka-k87@yandex.ru>
2022-01-28 03:21:59 +0300

    7+

--- 07-anagram/script.js
+++ 07-anagram/script.js
@@ -5,14 +5,26 @@
  * Напишите функцию, проверяющую, являются ли две строки анаграммами друг друга
  * (регистр букв не имеет значения). Для простоты примите, что в этих строках
  * нет пробелов и знаков препинания.
- * 
-*/
+ *
+ */
 
 function anagram(str1, str2) {
-    // Напишите код здесь
+  const str1Letters = str1.toLowerCase().split("").sort().join("");
+  const str2Letters = str2.toLowerCase().split("").sort().join("");
+  if (typeof str1 !== "string" || typeof str2 !== "string") {
+    return "одно из выражений не является строкой";
+  }
+  if (
+    str1.length !== str2.length ||
+    str1.toLowerCase() === str2.toLowerCase() ||
+    str1Letters !== str2Letters
+  ) {
+    return false;
+  } else {
+    return true;
+  }
 }
-
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(anagram('finder', 'Friend')); // true
-console.log(anagram('hello', 'bye')); // false
+console.log(anagram("finder", "Friend")); // true
+console.log(anagram("hello", "bye")); // false
