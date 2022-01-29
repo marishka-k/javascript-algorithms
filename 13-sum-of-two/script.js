d854a8d7d57068c00df7cc9520f510788e1e6c98
Marina Kravchenko <marishka-k87@yandex.ru>
2022-01-29 14:14:15 +0300

    13+

--- 13-sum-of-two/script.js
+++ 13-sum-of-two/script.js
@@ -5,14 +5,36 @@
  * и целое число num. Функция должна вернуть true, если в переданном массиве
  * есть какие-то два числа, чья сумма равна num. Если же такой пары чисел нет,
  * функция должна вернуть false.
- * 
-*/
+ *
+ */
 
 function sumOfTwo(arr, sum) {
-    // Напишите код здесь
+  if (!Array.isArray(arr)) {
+    return "введите массив целых чисел";
+  }
+  if (typeof sum !== "number" || sum < 0 || sum - Math.floor(sum) !== 0) {
+    return "ведите целое неотрицательное число";
+  }
+  for (let i = 0; i <= arr.length; i++) {
+    for (let x = i + 1; x < arr.length; x++) {
+      const elementsSum = arr[i] + arr[x];
+      if (
+        typeof elementsSum !== "number" ||
+        elementsSum - Math.floor(elementsSum) !== 0 ||
+        arr[i] < 0
+      ) {
+        return "в массиве должны быть только целые неотрицательные числа";
+      }
+      if (elementsSum === sum) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
 console.log(sumOfTwo([1, 2, 3, 4, 5], 4)); // true (так как 1 + 3 === 4)
 console.log(sumOfTwo([1, 2, 3, 4, 5], 100)); // false
+console.log(sumOfTwo([2, 2, 3, 4, 5], 100)); // false
